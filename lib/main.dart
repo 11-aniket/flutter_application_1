c69c4c9bbfedabd06b44cf74c5afd2531ad23cea
Aniket Gupta <aniket.r.gupta@slrtce.in>
2022-06-26 12:48:37 +0530

    day 5 completed (animation)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/routes.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -19,12 +20,12 @@ class myfirstapp extends StatelessWidget {
         brightness: Brightness.light,
          primarySwatch: Colors.green,
          fontFamily: GoogleFonts.lato().fontFamily,
-         primaryTextTheme: GoogleFonts.latoTextTheme(),
+         primaryTextTheme: GoogleFonts.aclonicaTextTheme(),
       ),
       routes: {
          "/" :(context) => LoginPage(),
-        "/home" :(context) => HomePage(),
-         "/login" :(context) => LoginPage(),
+        MyRoutes.homeRoute:(context) => HomePage(),
+         MyRoutes.loginRoute :(context) => LoginPage(),
       },
      );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,60 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/routes.dart';
 
-class LoginPage extends StatelessWidget {
-  // const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("assets/images/login page.png",
-          fit: BoxFit.contain,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Welcome",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.w600,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/login page.png",
+            fit: BoxFit.contain,
+            height: 300,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                 decoration: InputDecoration(
-                  hintText: "Enter user name ",
-                  labelText: "Username",
-                 ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                 decoration: InputDecoration
-                 (
-                  hintText: "Enter password",
-                  labelText: "Password",
-                 ),
-                ),
-                SizedBox(
-                  height: 20,
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome, $name",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                   decoration: InputDecoration(
+                    hintText: "Enter user name ",
+                    labelText: "Username",
+                   ),
+                   onChanged: (value) {
+                    name = value;
+                    setState(() {
+                      
+                    });
+                   },
                   ),
-                ElevatedButton(
-                  onPressed: () {
-                    
-                  }, 
-                  child: Text("Login"),
-                  style: TextButton.styleFrom(backgroundColor: Colors.amber)
+                  TextFormField(
+                    obscureText: true,
+                   decoration: InputDecoration
+                   (
+                    hintText: "Enter password",
+                    labelText: "Password",
+                   ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                    ),
+                  InkWell(
+                    onTap: (()async{
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
+                       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    }),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton ? 50 : 100,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: changeButton
+                          ? Icon(
+                            Icons.done,
+                            color: Colors.white70,
+                          )
+                          : Text(
+                        "Login",
+                         style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                           color: Color.fromARGB(255, 117, 51, 250),
+                           borderRadius: BorderRadius.circular( changeButton ? 30 :50),
+                        ),
+                    ),
                   )
-              ],
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   }, 
+                  //   child: Text("Login"),
+                  //   style: TextButton.styleFrom(
+                  //     minimumSize: Size(150, 40) )
+                  //   )
+                  
+                ],
+              ),
             ),
+          ],
           ),
-        ],
-        ),
+      ),
     );
   }
 }
